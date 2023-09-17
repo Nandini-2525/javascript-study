442aa1883760fa97a404532a3dee77278d52b2eb
Nandini Bodkhe <nandinibodkhe25@gmail.com>
2023-09-17 12:11:38 +0530

    function keywords

---
+++ funKey.js
@@ -0,0 +1,14 @@
+function greet(username){
+    console.log('Good Morning ' + username)
+}
+
+greet('Pralhad')
+greet('Nandini')
+greet('Savita')
+
+function add(a,b){
+    return a + b
+}
+const sum = add(5,10)
+console.log(sum)
+
