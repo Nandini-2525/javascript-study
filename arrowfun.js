2469adfa9b334559ae8f6f87f853ae52a13ae7ad
Nandini Bodkhe <nandinibodkhe25@gmail.com>
2023-09-17 12:13:07 +0530

    arrow function keywords

---
+++ arrowfun.js
@@ -0,0 +1,11 @@
+const arrowsum = ( a, b) => {
+    return a + b   
+}
+const sum = arrowsum(25, 25)
+console.log(sum)
+
+
+//another way is
+const arrSum = (a,b) => a + b
+const sum1 = arrSum(35, 25)
+console.log(sum1)
